90591c220f458286d1630d2c5615ff49fa17d40a
Ivan_Martsinkevich <martinkevi4@yandex.rum>
2023-11-22 20:40:17 +0300

    remove hardcoded shipping address

--- src/main/java/com/prestashop/demo/pages/checkout/AddressesPage.java
+++ src/main/java/com/prestashop/demo/pages/checkout/AddressesPage.java
@@ -2,6 +2,7 @@ package com.prestashop.demo.pages.checkout;
 
 import com.prestashop.demo.pages.AbstractPage;
 import org.openqa.selenium.By;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import static com.codeborne.selenide.Selenide.$;
@@ -9,15 +10,22 @@ import static com.codeborne.selenide.Selenide.$;
 @Component
 public class AddressesPage extends AbstractPage {
 
-    private final By address = By.xpath("//input[@id='field-address1']");
-    private final By zipCode = By.xpath("//input[@id='field-postcode']");
-    private final By city = By.id("field-city");
+    @Value(("${address}"))
+    protected String address;
+    @Value(("${city}"))
+    protected String city;
+    @Value(("${zipcode}"))
+    protected String zipcode;
+
+    private final By addressBy = By.xpath("//input[@id='field-address1']");
+    private final By zipCodeBy = By.xpath("//input[@id='field-postcode']");
+    private final By cityBy = By.id("field-city");
     private final By continueCTA = By.xpath("//div[@id='delivery-address']//button[@class='continue btn btn-primary float-xs-right']");
 
-    public void fillFieldWithValidData(){
-        $(address).sendKeys("41 Rue du Sergent Bobillot");
-        $(zipCode).sendKeys("93100");
-        $(city).sendKeys("Paris");
+    public void fillFieldWithValidData() {
+        $(addressBy).sendKeys(address);
+        $(zipCodeBy).sendKeys(zipcode);
+        $(cityBy).sendKeys(city);
         $(continueCTA).click();
     }
 
